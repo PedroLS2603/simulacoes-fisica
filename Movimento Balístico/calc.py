4a9deb0b9a6375bc33f1312d35470e947c193769
Pedro Lima Silva <pedro.l2002@hotmail.com>
2022-09-22 18:46:54 -0300

    estrutura de arrasto feita, falta corrigir equação

--- Movimento Balístico/calc.py
+++ Movimento Balístico/calc.py
@@ -1,44 +1,109 @@
 import math as m
 
-gravidade = 9.80665
+gravidade = 9.80665 #m/s^2
+densidade_ar = 1.2754 #kg/m^3
 
 # ** Equações eixo X ** #
 
-def v0_x(v_inicio, ang_inicio):
-    ang_inicio = m.radians(ang_inicio)
-    cos_ang = round(m.cos(ang_inicio), 6)
+class semResistencia:
+    def v0_x(v_inicio, ang_inicio):
+        ang_inicio = m.radians(ang_inicio)
+        cos_ang = round(m.cos(ang_inicio), 6)
 
-    return round(v_inicio * cos_ang, 6)
+        return round(v_inicio * cos_ang, 6)
 
-def pos_x(tempo, ang_inicio, v_inicio):
-    vel = v0_x(v_inicio, ang_inicio)
+    def pos_x(tempo, ang_inicio, v_inicio):
+        vel = semResistencia.v0_x(v_inicio, ang_inicio)
 
-    return round(vel * tempo, 4)
+        return round((vel * tempo), 4)
 
-# ** Equações eixo y ** #
+    # ** Equações eixo y ** #
 
-def v0_y(v_inicio, ang_inicio):
-    ang_inicio = m.radians(ang_inicio)
-    sen_ang = round(m.sin(ang_inicio), 6)
+    def v0_y(v_inicio, ang_inicio):
+        ang_inicio = m.radians(ang_inicio)
+        sen_ang = round(m.sin(ang_inicio), 6)
 
-    return round(v_inicio * sen_ang, 6)
+        return round(v_inicio * sen_ang, 6)
 
-def v_y(v_inicio, ang_inicio, tempo):
-    v0y = v0_y(v_inicio, ang_inicio)
+    def v_y(v_inicio, ang_inicio, tempo):
+        v0y = semResistencia.v0_y(v_inicio, ang_inicio)
 
-    return round(v0y - round(gravidade * tempo, 6), 6)
+        return round(v0y - round(gravidade * tempo, 6), 6)
 
-def pos_y(tempo, ang_inicio, inicio, v_inicio):
-    v0y = v0_y(v_inicio, ang_inicio)
+    def pos_y(tempo, ang_inicio, inicio, v_inicio):
+        v0y = semResistencia.v0_y(v_inicio, ang_inicio)
 
-    return round(inicio + v0y * tempo - ((gravidade * (m.pow(tempo, 2)))/2), 4)
+        return round(inicio + v0y * tempo - ((gravidade * (m.pow(tempo, 2)))/2), 4)
 
-# **      Outros     ** #
+    # **      Outros     ** #
 
-def ang(v_x, v_y):
-    tg_ang = v_y / v_x
+    def ang(v_x, v_y):
+        tg_ang = v_y / v_x
 
-    return m.degrees(m.atan(tg_ang))
+        return m.degrees(m.atan(tg_ang))
 
-def v_t(v_inicio, ang_inicio, tempo):
-    return round(m.sqrt(m.pow(v0_x(v_inicio, ang_inicio), 2) + m.pow(v_y(v_inicio, ang_inicio, tempo), 2)), 4)
+    def v_t(v_inicio, ang_inicio, tempo):
+        return round(m.sqrt(m.pow(semResistencia.v0_x(v_inicio, ang_inicio), 2) + m.pow(semResistencia.v_y(v_inicio, ang_inicio, tempo), 2)), 4)
+
+
+class comResistencia:
+
+    def area_circulo(diametro):
+        return round((m.pi * diametro ** 2)/4, 6)
+
+    def coef_arrasto():
+        area = comResistencia.area_circulo(1)
+        return round(0.5 * 0.47 * densidade_ar * area, 6)
+
+    # Equações eixo x
+    def v0_x(v_inicio, ang_inicio):
+        ang_inicio = m.radians(ang_inicio)
+        cos_ang = round(m.cos(ang_inicio), 6)
+
+        return round(v_inicio * cos_ang, 6)
+
+    def v_x(massa, v_inicio, ang_inicio, tempo):
+        v0 = comResistencia.v0_x(v_inicio, ang_inicio)
+        ca = comResistencia.coef_arrasto()
+
+        return round( v0 * m.e ** (ca / massa) * tempo, 4)
+
+    def pos_x(massa, tempo, ang_inicio, v_inicio):
+        v0 = comResistencia.v0_x(v_inicio, ang_inicio)
+        ca = comResistencia.coef_arrasto()
+
+        return round((v0/ca) * ( 1 - m.e ** ((-ca * tempo))), 4)
+
+    # ** Equações eixo y ** #
+
+    def v0_y(v_inicio, ang_inicio):
+        ang_inicio = m.radians(ang_inicio)
+        sen_ang = round(m.sin(ang_inicio), 6)
+
+        return round(v_inicio * sen_ang, 6)
+
+    def v_y(massa, v_inicio, ang_inicio, tempo):
+        v0y = comResistencia.v0_y(v_inicio, ang_inicio)
+        ca = comResistencia.coef_arrasto()
+
+        return round(((v0y + ((gravidade * massa)/ca)) * m.e ** (-(ca/massa) * tempo)) - ((gravidade * massa)/ca), 6)
+
+    def pos_y(massa, tempo, ang_inicio, v_inicio):
+        v0y = comResistencia.v0_y(v_inicio, ang_inicio)
+        ca = comResistencia.coef_arrasto()
+
+
+        return round(((massa/ca) * (v0y + (gravidade * massa)/ca) * (1 - m.e ** (-(ca/massa) * tempo))) - (((gravidade * massa)/ca) * tempo), 4)
+
+    # **      Outros     ** #
+
+    def ang(v_x, v_y):
+        tg_ang = v_y / v_x
+
+        if(v_x == 0):
+            return 0
+
+        return m.degrees(m.atan(tg_ang))
+
+    def v_t(massa, v_inicio, ang_inicio, tempo):
+        return round(m.sqrt(m.pow(comResistencia.v_x(massa, v_inicio, ang_inicio, tempo), 2) + m.pow(comResistencia.v_y(massa, v_inicio, ang_inicio, tempo), 2)), 4)
--- Movimento Balístico/mb.py
+++ Movimento Balístico/mb.py
@@ -1,12 +1,14 @@
 import matplotlib.pyplot as plt
 from IPython.display import display
 import pandas as pd
-import calc as c
+from calc import semResistencia as sr
+from calc import comResistencia as cr
 
 ang_inicio = float(input("Digite o ângulo: "))
 v_inicio = float(input("Digite a velocidade inicial: "))
 inicio = float(input("Digite a posição inicial: "))
 intervalo = 0
+massa = 2
 
 intervalos = []
 posicoes_x = []
@@ -18,18 +20,28 @@ comp_vx = []
 comp_vy = []
 angs = []
 
+intervalos2 = []
+posicoes_x2 = []
+posicoes_y2 = []
+vel_x2 = []
+vel_y2 = []
+vel_t2 = []
+comp_vx2 = []
+comp_vy2 = []
+angs2 = []
+
 while(intervalo >= 0):
     
-    pos_x = c.pos_x(intervalo, ang_inicio, v_inicio)
-    pos_y = c.pos_y(intervalo, ang_inicio, inicio, v_inicio)
-    v0x = c.v0_x(v_inicio, ang_inicio)
-    v0y = c.v0_y(v_inicio, ang_inicio)
+    pos_x = sr.pos_x(intervalo, ang_inicio, v_inicio)
+    pos_y = sr.pos_y(intervalo, ang_inicio, inicio, v_inicio)
+    v0x = sr.v0_x(v_inicio, ang_inicio)
+    v0y = sr.v0_y(v_inicio, ang_inicio)
     vx = v0x #velocidade constante
-    vy = c.v_y(v_inicio, ang_inicio, intervalo)
-    vt = c.v_t(v_inicio, ang_inicio, intervalo)
+    vy = sr.v_y(v_inicio, ang_inicio, intervalo)
+    vt = sr.v_t(v_inicio, ang_inicio, intervalo)
 
     if intervalo > 0:
-        ang = c.ang(c.v0_x(v_inicio, ang_inicio), c.v_y(v_inicio, ang_inicio, intervalo))
+        ang = sr.ang(sr.v0_x(v_inicio, ang_inicio), sr.v_y(v_inicio, ang_inicio, intervalo))
     else:
         ang = ang_inicio
 
@@ -43,21 +55,55 @@ while(intervalo >= 0):
     vel_t.append(vt)
     angs.append(ang)
 
-    if intervalo > 0 and pos_y < 0:
+    if intervalo > 0 and pos_y <= 0:
         break
 
     intervalo += 0.1
 
+intervalo = 0
+
+
+while (intervalo >= 0):
+    pos_x2 = cr.pos_x(massa, intervalo, ang_inicio, v_inicio)
+    pos_y2 = cr.pos_y(massa, intervalo, ang_inicio, v_inicio)
+    v0x2 = cr.v0_x(v_inicio, ang_inicio)
+    v0y2 = cr.v0_y(v_inicio, ang_inicio)
+    vx2 = cr.v_x(massa, v_inicio, ang_inicio, intervalo)
+    vy2 = cr.v_y(massa, v_inicio, ang_inicio, intervalo)
+    vt2 = cr.v_t(massa, v_inicio, ang_inicio, intervalo)
+
+    if intervalo > 0:
+        ang2 = cr.ang(cr.v_x(massa, v_inicio, ang_inicio, intervalo), cr.v_y(massa, v_inicio, ang_inicio, intervalo))
+    else:
+        ang2 = ang_inicio
+
+
+    posicoes_x2.append(pos_x2)
+    posicoes_y2.append(pos_y2)
+    comp_vx2.append(v0x2)
+    comp_vy2.append(v0y2)
+    vel_x2.append(vx2)
+    vel_y2.append(vy2)
+    vel_t2.append(vt2)
+    angs2.append(ang2)
+
+    if intervalo > 0 and pos_y2 <= 0:
+        break
+
+    intervalo += 0.1
 
 plt.figure('Movimento balístico')
-plt.ylabel('Altura')
-plt.xlabel('Distância')
+plt.ylabel('Altura (m)')
+plt.xlabel('Distância (m)')
 plt.plot(posicoes_x, posicoes_y)
+plt.plot(posicoes_x2, posicoes_y2)
+plt.legend(["Sem resistência", "Com resistência"])
 plt.xlim(left = -1)
 
 planilha = pd.DataFrame({'T (s)': intervalos, 'X (m)':posicoes_x, 'Y (m)': posicoes_y, 'V0x (m/s)': comp_vx, 'V0y (m/s)': comp_vy, 'Vx (m/s)': vel_x, 'Vy (m/s)': vel_y, 'Vt (m/s)': vel_t, 'α (°)': angs})
 
 display(planilha)
+
 
 plt.show()
 
